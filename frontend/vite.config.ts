da1b10a34068afd23abb64baa322422f0cdfb65e
Vignesh G <kuttydravid463@gmail.com>
2026-03-16 20:44:31 +0530

    vite configurations updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -16,6 +15,10 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      tslib: path.resolve(__dirname, "node_modules/tslib/tslib.es6.js"),
     },
+  },
+  optimizeDeps: {
+    include: ["tslib"],
   },
 }));
